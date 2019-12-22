15fd41d986090fc5506ec2ab02cc700b0e9e7d5b
zgpeace <zgpeace@gmail.com>
2019-12-22 18:54:37 +0800

    Initial commit

---
+++ Podfile
@@ -0,0 +1,12 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '12.0'
+
+target 'RWPickFlavor' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for RWPickFlavor
+  pod 'Alamofire', '~> 4.7'
+  pod 'MBProgressHUD', '~> 1.1.0', :modular_headers => true
+
+end
---
+++ RWPickFlavor/RWPickFlavor.h
@@ -0,0 +1,19 @@
+//
+//  RWPickFlavor.h
+//  RWPickFlavor
+//
+//  Created by zgpeace on 2019/12/21.
+//  Copyright © 2019 zgpeace. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for RWPickFlavor.
+FOUNDATION_EXPORT double RWPickFlavorVersionNumber;
+
+//! Project version string for RWPickFlavor.
+FOUNDATION_EXPORT const unsigned char RWPickFlavorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RWPickFlavor/PublicHeader.h>
+
+
